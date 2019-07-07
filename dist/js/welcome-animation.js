69bc6f1d0dca4ac805fef0ff2a435d8f8f5627b9
twentyshaw <isherlock951106@gmail.com>
2019-07-07 22:44:09 +0900

    使用了webpack

--- dist/js/welcome-animation.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var cancel_a = function cancel_a() {
-  siteWelcome.classList.remove("active");
-};
-
-setTimeout(cancel_a, 2000);
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/js/app.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist/js/')
+  },
+  mode: 'development',
+  module: {
+  rules: [
+    {
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    },
+    {
+            test: /\.scss$/,
+            use: [
+                "style-loader", // creates style nodes from JS strings
+                "css-loader", // translates CSS into CommonJS
+                "sass-loader" // compiles Sass to CSS, using Node Sass by default
+            ]
+    },
+    {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+    }
+  ]
+  }
+};
